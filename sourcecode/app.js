d0375ac478b61c5e26284bb9b082fcc6b6884bcd
Shashank Agarwal <shashank1010@gmail.com>
2018-10-06 17:22:00 +0530

    React with Babel 7 and Webpack 4

---
+++ sourcecode/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+	return <div>React is Good</div>
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
